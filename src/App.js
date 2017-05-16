2a212e26fd8d60e21351be7938560682632f48d3
ethankhoa <ethan.huynh@gmail.com>
2017-05-16 15:27:49 +1000

    adds retry quiz function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import Question from './Question';
 import ProgressBar from './ProgressBar';
 import MultiChoice from './MultiChoice';
+import Results from './Results';
 
 class App extends Component {
   constructor(props) {
@@ -15,6 +16,7 @@ class App extends Component {
     };
     this.updateSelected = this.updateSelected.bind(this); // bind to allow access in child
     this.submitAnswer = this.submitAnswer.bind(this);
+    this.restartQuiz = this.restartQuiz.bind(this);
     this.quiz_data = [
       {
         question: "What is the name of the diner/restaurant?",
@@ -48,10 +50,17 @@ class App extends Component {
     }
   }
 
-
   updateSelected(answer) {
     this.setState({ // () because function {} because passing object
       selected: answer
+    })
+  }
+
+  restartQuiz() {
+    this.setState({
+      progress: 0,
+      selected: 'None yet!',
+      score: 0
     })
   }
 
@@ -59,12 +68,19 @@ class App extends Component {
     return (
       <div>
         <h2>Seinfeld Quiz App</h2>
-        <Question current_question={this.quiz_data[this.state.progress].question} /> {/* current_step, question_length are all props */}
-        <ProgressBar current_step={this.state.progress + 1}
-          question_length={this.quiz_data.length} />
-        <MultiChoice answers={this.quiz_data[this.state.progress].possible_answers}
-        updateSelected={this.updateSelected} handleSubmit={this.submitAnswer}
-        selectedAnswer={this.state.selected} />
+        {this.state.progress < this.quiz_data.length ? (
+          <div>
+            <Question current_question={this.quiz_data[this.state.progress].question} />
+            <ProgressBar current_step={this.state.progress + 1}
+              question_length={this.quiz_data.length} />
+            <MultiChoice answers={this.quiz_data[this.state.progress].possible_answers}
+            updateSelected={this.updateSelected} handleSubmit={this.submitAnswer}
+            selectedAnswer={this.state.selected} />
+          </div>
+        )
+        : (
+          <Results score={this.state.score} end_message="Congrats! You're done!" restartQuiz={this.restartQuiz} />
+        )}
       </div>
     );
   }
